6a6800377543cbb58880981c37ecf114ee55639e
nottombrown <nottombrown@gmail.com>
2018-01-30 17:59:03 -0800

    Bump version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='imagenet-stubs',
-      version='0.0.2',
+      version='0.0.3',
       author='Tom Brown',
       author_email='nottombrown@gmail.com',
       packages=['imagenet_stubs'],
