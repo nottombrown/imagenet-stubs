66a518d924aa8ac389ee95a6415c2791c306c060
nottombrown <nottombrown@gmail.com>
2018-01-30 18:29:03 -0800

    Include images

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
 from setuptools import setup
 
 setup(name='imagenet-stubs',
-      version='0.0.5',
+      version='0.0.6',
       author='Tom Brown',
       author_email='nottombrown@gmail.com',
       packages=['imagenet_stubs'],
+      package_data={'': ['imagenet_stubs/images/*']},
+      include_package_data=True,
      )
